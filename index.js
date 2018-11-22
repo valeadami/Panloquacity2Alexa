1e77a234425450084786680cdee944796eb7a7ac
Valentina Adami <valeadami@gmail.com>
2018-11-22 14:32:03 +0100

    refactor callAva

--- index.js
+++ index.js
@@ -49,191 +49,81 @@ server.listen(port, function () {
 });
 
 alexaRouter.post('/callAVA', function (req, res) {
-    console.log('sono in callAVA ');
+    console.log('sono in  post /callAVA ');
     bot=req.query.ava;
-   /* let request = req.body.request;
-    console.log(JSON.stringify(request));*/
-  
-   
-   
-    if (req.body.request.type === 'LaunchRequest') {
-      /*
-        res.json({
-            "version": "1.0",
-            "response": {
-              "shouldEndSession": false,
-              "outputSpeech": {
-                "type": "PlainText",
-                "text": "Benvenuto in Panloquacity"
-              }
-            }
-          });    */
+  //modifica del 22/11/2018
+    callAva(req,res);
+  /*
+   if (req.body.request.type === 'LaunchRequest') {
+      
           callAvaLaunchRequest(req,res);
     }
     else if (req.body.request.type === 'IntentRequest' &&
              req.body.request.intent.name === 'AnyText') {     
-       // BuildGetCookingInstruction(req, res); 
+       
             callAva(req, res);
          
         
     } else if (req.body.request.type === 'IntentRequest'  && req.body.request.intent.name === 'AMAZON.HelpIntent') { 
         console.log('Hai chiesto aiuto');
         callAva(req, res);
-      // res.json({
-      //   "version": "1.0",
-      //   "response": {
-      //     "shouldEndSession": false,
-      //     "outputSpeech": {
-      //       "type": "PlainText",
-      //       "text": "Puoi chiedermi quello che vuoi"
-      //     }
-      //   }
-      // });  
+      
     } else if (req.body.request.type === 'IntentRequest'  && req.body.request.intent.name === 'AMAZON.StopIntent') { 
         console.log('Vuoi uscire');
-        callAva(req, res);
-      // res.json({
-      //   "version": "1.0",
-      //   "response": {
-      //     "shouldEndSession": true,
-      //     "outputSpeech": {
-      //       "type": "PlainText",
-      //       "text": "Ok, chiudo la sessione. Quando vuoi, dì Alexa apri provaslot"
-      //     }
-      //   }
-      // });  
+     
     } else if (req.body.request.type === 'IntentRequest'  && req.body.request.intent.name === 'AMAZON.CancelIntent') { 
         console.log('Vuoi annullare');
         callAva(req, res);
-        // res.json({
-        //     "version": "1.0",
-        //     "response": {
-        //       "shouldEndSession": false,
-        //       "outputSpeech": {
-        //         "type": "PlainText",
-        //         "text": "Ok, annullo"
-        //       }
-        //     }
-        //   });  
+          
     } else if (req.body.request.type === 'SessionEndedRequest') { 
         console.log('Session ended', req.body.request.reason);
         callAva(req, res);
-        // res.json({
-        //     "version": "1.0",
-        //     "response": {
-        //       "shouldEndSession": true,
-        //       "outputSpeech": {
-        //         "type": "PlainText",
-        //         "text": "Arrivederci"
-        //       }
-        //     }
-        //   }); 
-    } 
+       
+    } */
     
 });
-//AMAZON.CancelIntent
 
+/* modifica del 22/11/2018 
+Passo a PLQ queste strunghe per gestire:
+- intent di "welcome": passo "zzzstart" all'avvio della conversazione ossia al LaunchRequest->invocazione di Alexa
+- intent di StopIntent: passo "zzzstop" quando utente dice "basta"
+- intent di Cancel: passo "zzzannulla" quando utente dice "annulla"
+- intent di Help: passo "zzzhelp" quando utente dice "aiuto"
+- intent di Fallback: passo "zzzNoResponse" 
+- intent di SessionEnded: passo "zzzEndSession"
+*/
 function callAva(req, resp){
-    let request = req.body.request;
-    let strRicerca='';
-    let out='';
-    let data='';
-    let strOutput='';
-    let sessionId = req.body.session.sessionId;
-    //bot=req.query.ava;
-    console.log('sessionID di Alexa= ' + sessionId);
-    //prendo il parametro....slot 
-    var str=request.intent.slots.searchText.value;
-        if(str) {
-            strRicerca = querystring.escape(str);;
-           
-            options.path+=strRicerca+'&user=&pwd=&ava='+bot;
-            console.log('stringa ricerca  = '+ strRicerca + " bot interrogato "+bot);
-        }
-        var ss=leggiSessione(__dirname +'/sessions/', sessionId);
-        if (ss===''){
-            options.headers.Cookie='JSESSIONID=';
-            console.log('DENTRO CALL AVA: SESSIONE VUOTA');
-        }else {
-             options.headers.Cookie='JSESSIONID='+ss;
-             console.log('DENTRO CALL AVA:  HO LA SESSIONE + JSESSIONID');
-        }
-        
-        var req1 = http.request(options, (res) => {
-             
-            console.log('________valore di options.cookie INIZIO ' + options.headers.Cookie);
-            console.log(`STATUS DELLA RISPOSTA: ${res.statusCode}`);
-            console.log(`HEADERS DELLA RISPOSTA: ${JSON.stringify(res.headers)}`);
-            console.log('..............RES HEADER ' + res.headers["set-cookie"] );
-           
-            if (res.headers["set-cookie"]){
-        
-              var x = res.headers["set-cookie"].toString();
-              var arr=x.split(';')
-              var y=arr[0].split('=');
-              
-             console.log('id di sessione di ava =' + y[1]);
-             
-             scriviSessione(__dirname+'/sessions/',sessionId, y[1]); 
-            } 
-            res.setEncoding('utf8');
-            res.on('data', (chunk) => {
-             console.log(`BODY: ${chunk}`);
-             data += chunk;
-           
-             let c=JSON.parse(data);
-                    strOutput=c.output[0].output; 
-                   
-                    strOutput=strOutput.replace(/(<\/p>|<p>|<b>|<\/b>|<br>|<\/br>|<strong>|<\/strong>|<div>|<\/div>|<ul>|<li>|<\/ul>|<\/li>|&nbsp;|)/gi, '');
-                    resp.json({
-                        "version": "1.0",
-                        "response": {
-                            "shouldEndSession": false,
-                            "outputSpeech": {
-                            "type": "PlainText",
-                            "text": strOutput
-                            }
-                        }
-                    }); 
-                  
-                  
-            });
-            res.on('end', () => {
-              console.log('No more data in response.');
-              
-                   
-                    options.path='/AVA/rest/searchService/search_2?searchText=';
-                    
-                    console.log('valore di options.path FINE ' +  options.path);
-        
-            });
-          });
-          
-          req1.on('error', (e) => {
-            console.error(`problem with request: ${e.message}`);
-            strOutput="si è verificato errore " + e.message;
-           
-          });  
-          
-        req1.write(postData);
-        req1.end();
-        
-};
-
-function callAvaLaunchRequest(req, resp){
-  console.log('sono in callAvaLaunchRequest');
-  //let request = req.body.request;
-  let strRicerca='zzzstart';
-  let out='';
+  console.log('**************** sono in FUNZIONE callAva modificato');
+  
+  let strRicerca='';
   let data='';
   let strOutput='';
   let sessionId = req.body.session.sessionId;
+  let request = req.body.request; //per slot
   //bot=req.query.ava;
   console.log('sessionID di Alexa= ' + sessionId);
+
+  if (req.body.request.type === 'LaunchRequest') {
+      strRicerca='zzzstart';
+  } else if (req.body.request.type === 'IntentRequest' &&
+    req.body.request.intent.name === 'AnyText') {     
+      strRicerca=request.intent.slots.searchText.value; 
+      strRicerca = querystring.escape(strRicerca); 
+    } else if (req.body.request.type === 'IntentRequest'  && req.body.request.intent.name === 'AMAZON.HelpIntent') {
+      strRicerca='zzzhelp';
+    } else if (req.body.request.type === 'IntentRequest'  && req.body.request.intent.name === 'AMAZON.StopIntent') { 
+      strRicerca='zzzstop';
+    } else if (req.body.request.type === 'IntentRequest'  && req.body.request.intent.name === 'AMAZON.CancelIntent') { 
+      strRicerca='zzzannulla';
+    } else if (req.body.request.type === 'SessionEndedRequest') {
+      strRicerca='zzzEndSession';
+    } else if (req.body.request.intent.name === 'AMAZON.FallbackIntent') {  
+      strRicerca='zzzNoResponse';
+    }
   //prendo il parametro....slot 
-  //var str=request.intent.slots.searchText.value;
+    //var str=request.intent.slots.searchText.value;
       if(strRicerca) {
-         // strRicerca = querystring.escape(str);;
+         
          
           options.path+=strRicerca+'&user=&pwd=&ava='+bot;
           console.log('stringa ricerca  = '+ strRicerca + " bot interrogato "+bot);
@@ -307,6 +197,7 @@ function callAvaLaunchRequest(req, resp){
       req1.end();
       
 };
+
 /**** FUNZIONI A SUPPORTO */
 
 function scriviSessione(path, strSessione, strValore) {
