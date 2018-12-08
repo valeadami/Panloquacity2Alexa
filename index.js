70ad488ac8c48d36710cd3ab9640b249f9cd5006
Valentina Adami <valeadami@gmail.com>
2018-12-08 22:11:29 +0100

    con https

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var express = require('express');
 //07/12/2018 aggiunto questo modulo per certificazione alexa
 var verifier = require('alexa-verifier-middleware') 
 var bodyParser = require('body-parser');
-var http = require('http');
+var http = require('https');
 var querystring = require('querystring');
 var path = require("path");
 var fs = require("fs");
@@ -40,7 +40,7 @@ const options = {
     //modifica del 12/11/2018 : cambiato porta per supportare HTTPS
     
   hostname: '86.107.98.69', 
-  port: 8080,
+  port: 8443,
    /*port: 8443, 8080*/
    //rejectUnauthorized: false, // aggiunto qui 12/11/2018 
    path: '/AVA/rest/searchService/search_2?searchText=', 
@@ -114,7 +114,7 @@ function callAva(req, resp){
   //bot=req.query.ava;
   console.log('***************headers della richiesta = '+ JSON.stringify(req.headers));
   console.log('sessionID di Alexa= ' + sessionId);
-  
+
   if (req.body.request.type === 'LaunchRequest') {
       strRicerca='zzzstart';
   } else if (req.body.request.type === 'IntentRequest' &&
